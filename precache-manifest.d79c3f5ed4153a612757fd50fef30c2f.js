12e3fcec920cf15d640750bc958af9538914b30b
Jagan <peddinti.jagannadh@algoworks.com>
2020-03-08 18:59:33 +0530

    Updates

---
+++ precache-manifest.d79c3f5ed4153a612757fd50fef30c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d83653441a15ebd5d521c692a1909e",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "8cb1b1ee20712f9ea2b3",
+    "url": "/react-rolodex/static/css/main.757b320e.chunk.css"
+  },
+  {
+    "revision": "025ca7457249d74d40ff",
+    "url": "/react-rolodex/static/js/2.c8e6a317.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-rolodex/static/js/2.c8e6a317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb1b1ee20712f9ea2b3",
+    "url": "/react-rolodex/static/js/main.72106196.chunk.js"
+  },
+  {
+    "revision": "9f2091a0e5dfd241d60b",
+    "url": "/react-rolodex/static/js/runtime-main.dda1060c.js"
+  }
+]);
